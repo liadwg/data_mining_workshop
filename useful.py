cfb870f19b8bef58189831617891c9de70bfbae8
guysalame <guy.salame@planckresolution.com>
2017-03-08 14:15:22 +0200

    Added new_photo file

---
+++ useful.py
@@ -0,0 +1,7 @@
+
+#group by
+tips = all_data.groupby(['price_range'], as_index=False).count()[['price_range', 'likes']]
+
+#rename
+tips.rename(columns={'likes':'cnt'}, inplace=True)
+all_data.price_range.map({0:'None', 1:'1$', 2:'2$', 3:'3$', 4:'4$'})

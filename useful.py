20893eb860e7029db91f760943d4cea0a9db6804
guysalame <guy.salame@planckresolution.com>
2017-03-08 18:50:01 +0200

    salame notebook

--- useful.py
+++ useful.py
@@ -5,3 +5,6 @@ tips = all_data.groupby(['price_range'], as_index=False).count()[['price_range',
 #rename
 tips.rename(columns={'likes':'cnt'}, inplace=True)
 all_data.price_range.map({0:'None', 1:'1$', 2:'2$', 3:'3$', 4:'4$'})
+
+
+all_data[all_data['has_profile_photo'] < 1].shape[0]
